89b9b8a5dd08ba68e6b6051a5c2915c96b1c6c5e
S Borkar <f20170928@goa.bits-pilani.ac.in>
2020-08-03 02:36:44 +0530

    Updates

---
+++ precache-manifest.acdc1090a06e2026a721f2986055c9ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c56fe9160a39b06140d892f5e8782e",
+    "url": "/React-2048/index.html"
+  },
+  {
+    "revision": "c211d6d200c8d102342e",
+    "url": "/React-2048/static/css/main.6b4c42bd.chunk.css"
+  },
+  {
+    "revision": "487b44b130e6c3b9d666",
+    "url": "/React-2048/static/js/2.e36f67d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-2048/static/js/2.e36f67d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c211d6d200c8d102342e",
+    "url": "/React-2048/static/js/main.75405e12.chunk.js"
+  },
+  {
+    "revision": "3a14c9cda75e6e7546e0",
+    "url": "/React-2048/static/js/runtime-main.63a82d79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-2048/static/media/logo.5d5d9eef.svg"
+  }
+]);
